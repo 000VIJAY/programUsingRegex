a996967b80e90bc449808d789a5b7f2bf0da3fef
000VIJAY <vijay893689@gmail.com>
2022-07-01 16:02:34 +0530

    Added Program for validate password is valid or not

--- ProblemUsingRegex/Pattern.cs
+++ ProblemUsingRegex/Pattern.cs
@@ -13,6 +13,7 @@ namespace ProblemUsingRegex
         public string validLastName = @"^[A-Z][a-z]{3,10}$";
         public string validEmailId = @"^[A-Za-z0-9._]+@[A-Za-z0-9]+.[a-z]{2,5}$";
         public string validMobileNumber = @"^[1-9]{1,3} +[6-9]{1}[0-9]{9}";
+        public string validPassword = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8}$";
         public bool ValidateFirstName(string firstName )
         {
             return Regex.IsMatch( firstName, validFirstName );
@@ -28,6 +29,10 @@ namespace ProblemUsingRegex
         public bool ValidateMobileNumber(string moblieNumber)
         {
             return Regex.IsMatch( moblieNumber, validMobileNumber);
+        }
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, validPassword);
         }
     }
 }
